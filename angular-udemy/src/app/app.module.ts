6bf7c0a1af597a187d4d6c16af0b17b13b2196f9
Andrii Veremeienko <illidan00000@gmail.com>
2022-04-16 14:29:40 +0300

    Set up Ngrx router-store

--- angular-udemy/src/app/app.module.ts
+++ angular-udemy/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EffectsModule } from '@ngrx/effects';
 import * as fromApp from './store/app.reducer';
 import * as AuthEffects from './auth/store/auth.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -28,7 +29,8 @@ import { environment } from '../environments/environment';
     CoreModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects.AuthEffects]),
-    StoreDevtoolsModule.instrument({logOnly: environment.production})
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot()
   ],
   bootstrap: [AppComponent],
   entryComponents: [
