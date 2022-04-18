87b7b8e0270553edff18746ffc8c3f2b8b02e7b5
Andrii Veremeienko <illidan00000@gmail.com>
2022-04-18 16:44:00 +0300

    Set up Angular Universal

--- angular-udemy/src/app/app.component.ts
+++ angular-udemy/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,15 @@ import * as AuthActions from './auth/store/auth.actions';
 export class AppComponent implements OnInit {
   title = 'angular-udemy';
 
-  constructor(private store: Store<fromApp.AppState>) { }
+  constructor(
+    private store: Store<fromApp.AppState>,
+    @Inject(PLATFORM_ID) private platformId  
+  ) { }
 
   ngOnInit(): void {
-    this.store.dispatch(new AuthActions.AutoLogin);
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin);
+    }
   }
 
 }
